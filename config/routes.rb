b989476fad13c18d61636533a8d3e1b8f689167f
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-10-04 15:31:30 +0000

    Devise追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+  namespace :public do
+    
+  end
+  devise_for :admin,skip: [:registrations, :passwords] , controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :members,controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
