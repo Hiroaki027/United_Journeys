e2822c7d2562d946f9e4bc4f79d880e3699ca250
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-10-05 08:33:57 +0000

    Model,Controller,gemの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'public/homes#top'
-  namespace :public do
-    
-  end
+
   devise_for :admin,skip: [:registrations, :passwords] , controllers: {
     sessions: "admin/sessions"
   }
@@ -12,4 +9,13 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope module: :public do
+    root to: 'homes#top'
+    resources :members,  only: [:show,:edit,:update,:withdrawal]
+    resources :posts, except: [:new] do
+      resources :post_comments, only: [:create,:destroy]
+      resource :favorites, only: [:create,:destroy]
+    end
+  end
 end
