d78bc76ee460d5ed06eaa0474eba5abaa2ebc2b2
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-10-09 14:03:26 +0000

    いいね投稿一覧

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,5 +1,6 @@
 class Public::MembersController < ApplicationController
   before_action :ensure_guest_user, only: [:edit] #下記に定義したensure_guest_userでurlからもeditへ遷移できないように制限
+
   def show
     @member = Member.find(params[:id])
   end
@@ -7,7 +8,7 @@ class Public::MembersController < ApplicationController
   def edit
     @member = current_member
   end
-  
+
   def update
     @member = current_member
     if @member.update(member_params)
@@ -26,15 +27,24 @@ class Public::MembersController < ApplicationController
     flash[:notice] = "退会処理を完了しました。ご利用ありがとうございました。"
     redirect_to root_path
   end
-  
+
+  def favorites
+    @member = Member.find(params[:id])
+    favorites = Favorite.where(member_id: @member.id).pluck(:post_id)
+    @favorite_posts = Post.find(favorites)
+  end
+
+  private
+
   def member_params
     params.require(:member).permit(:last_name, :first_name, :nick_name, :email, :introduction, :residence)
   end
-  
+
+
   def ensure_guest_user
     @member = Member.find(params[:id])
     if @member.guest_member?
       redirect_to member_path(current_member) , notice: "ゲスト会員はプロフィール編集画面へ遷移できません。"
     end
-  end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :members,  only: [:show,:edit,:update] do
       member do
         patch :withdrawal
+        get :favorites
       end
     end
     resources :posts do
