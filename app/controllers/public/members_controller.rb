0b27f96fa0222cb38068a8e3e49fa9b559e987b9
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-11-30 09:23:56 +0000

    URL遷移制限

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,6 +1,7 @@
 class Public::MembersController < ApplicationController
   before_action :authenticate_member!, except: [:top, :admin]
-  before_action :ensure_guest_user, only: [:edit] #下記に定義したensure_guest_userでurlからもeditへ遷移できないように制限
+  before_action :ensure_guest_member, only: [:edit] #下記に定義したensure_guest_userでurlからもeditへ遷移できないように制限
+  before_action :ensure_member, only: [:edit]
 
   def show
     @member = Member.find(params[:id])
@@ -48,7 +49,7 @@ class Public::MembersController < ApplicationController
   end
 
 
-  def ensure_guest_user
+  def ensure_guest_member
     @member = Member.find(params[:id])
     if @member.guest_member?
       redirect_to member_path(current_member) , notice: "ゲスト会員はプロフィール編集画面へ遷移できません。"
